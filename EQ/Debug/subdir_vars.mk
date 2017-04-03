6e482230a85c6780f686e063a4ad0906d7f9a545
FilipDutina <filip.dutina@gmail.com>
2017-04-04 00:09:27 +0200

    initial commit

---
+++ EQ/Debug/subdir_vars.mk
@@ -0,0 +1,65 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnkx.cmd 
+
+C_SRCS += \
+../aic3204.c \
+../aic3204_init.c \
+../ezdsp5535_aic3204_dma.c \
+../iir.c \
+../main.c \
+../print_number.c \
+../processing.c 
+
+C_DEPS += \
+./aic3204.d \
+./aic3204_init.d \
+./ezdsp5535_aic3204_dma.d \
+./iir.d \
+./main.d \
+./print_number.d \
+./processing.d 
+
+OBJS += \
+./aic3204.obj \
+./aic3204_init.obj \
+./ezdsp5535_aic3204_dma.obj \
+./iir.obj \
+./main.obj \
+./print_number.obj \
+./processing.obj 
+
+OBJS__QUOTED += \
+"aic3204.obj" \
+"aic3204_init.obj" \
+"ezdsp5535_aic3204_dma.obj" \
+"iir.obj" \
+"main.obj" \
+"print_number.obj" \
+"processing.obj" 
+
+C_DEPS__QUOTED += \
+"aic3204.d" \
+"aic3204_init.d" \
+"ezdsp5535_aic3204_dma.d" \
+"iir.d" \
+"main.d" \
+"print_number.d" \
+"processing.d" 
+
+C_SRCS__QUOTED += \
+"../aic3204.c" \
+"../aic3204_init.c" \
+"../ezdsp5535_aic3204_dma.c" \
+"../iir.c" \
+"../main.c" \
+"../print_number.c" \
+"../processing.c" 
+
+
